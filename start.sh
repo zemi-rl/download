da2ea1dab2ee3e45bbeee548e08d8fb8db3b8cd9
zemi-rl <ianmattas20@gmail.com>
2025-08-28 19:09:55 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+set -e
 
+# Create cert directory
+mkdir -p /app/certs
+
+# Decode certs from environment variables
+echo "$SERVER_KEY_B64" | base64 -d > /app/certs/server.key
+echo "$SERVER_CRT_B64" | base64 -d > /app/certs/server.crt
+echo "$CA_CRT_B64" | base64 -d > /app/certs/ca.crt
+
+# Start the Node.js app
+node server.js
